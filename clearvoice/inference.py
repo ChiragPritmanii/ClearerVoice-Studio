0c770c834134bdb5872dc1d2f8b3c0358ac095b4
pritmani.chirag@arrise.com <pritmani.chirag@arrise.com>
2026-01-28 19:16:04 +0530

    added a sample mp4

--- clearvoice/inference.py
+++ clearvoice/inference.py
@@ -6,7 +6,7 @@ def main():
     )
 
     myClearVoice(
-        input_path="/content/ClearerVoice-Studio/clearvoice/samples/path_to_input_videos_tse/bj06_split_00.mp4", 
+        input_path="/content/ClearerVoice-Studio/clearvoice/samples/path_to_input_videos_tse/blackjack01.mp4", 
         online_write=True, 
         output_path="/content/ClearerVoice-Studio/clearvoice/samples/path_to_output_videos_tse/"
         )
