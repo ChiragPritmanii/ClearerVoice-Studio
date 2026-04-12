7157d5485f53967fdffbd081aeab9534e7c293d0
Chirag Pritmani <cpritmani@gmail.com>
2026-04-12 23:43:47 +0530

    save directly to drive

--- clearvoice/inference.py
+++ clearvoice/inference.py
@@ -8,7 +8,8 @@ def main():
     myClearVoice(
         input_path="/content/ClearerVoice-Studio/clearvoice/samples/scp/video_samples.scp", 
         online_write=True, 
-        output_path="/content/ClearerVoice-Studio/clearvoice/samples/path_to_output_videos_tse/"
+        output_path="/content/drive/MyDrive/game-recordings/AV-BJ/clean_moss_2/",
+        # output_path="/content/ClearerVoice-Studio/clearvoice/samples/path_to_output_videos_tse/"
         )
     
 
